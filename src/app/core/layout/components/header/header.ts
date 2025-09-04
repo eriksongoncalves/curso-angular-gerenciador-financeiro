c06f4eeb94617a83354535c1f862bf699d70cd1f
Erikson Gonçalves Sanchez <erikson.sanchez@outlook.com>
2025-09-03 21:53:05 -0300

    feat: layout and header component

---
+++ src/app/core/layout/components/header/header.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { MatToolbar } from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-header',
+  imports: [MatToolbar],
+  templateUrl: './header.html',
+  styleUrl: './header.scss',
+})
+export class Header {}
