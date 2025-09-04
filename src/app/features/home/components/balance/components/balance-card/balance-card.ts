ad83a1dc9cfbef942261a5662842a7a1bd99adc4
Erikson Gonçalves Sanchez <eriksongoncalves@yahoo.com.br>
2025-09-04 16:20:12 -0300

    feat: balance component

--- src/app/features/home/components/balance/components/balance-card/balance-card.ts
+++ src/app/features/home/components/balance/components/balance-card/balance-card.ts
@@ -1,6 +1,9 @@
 import { Component, computed, input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 
+type BalanceCardType = 'income' | 'outcome' | 'balance';
+type CssClass = 'income' | 'outcome' | 'zero';
+
 @Component({
   selector: 'app-balance-card',
   imports: [MatCardModule],
@@ -8,15 +11,19 @@ import { MatCardModule } from '@angular/material/card';
   styleUrl: './balance-card.scss',
 })
 export class BalanceCard {
-  type = input.required<'income' | 'outcome' | 'balance'>();
+  type = input.required<BalanceCardType>();
   label = input.required<string>();
   amount = input.required<number>();
 
-  cssClass = computed(() => {
+  cssClass = computed<CssClass>(() => {
     if (this.type() === 'balance') {
+      if (this.amount() === 0) {
+        return 'zero';
+      }
+
       return this.amount() > 0 ? 'income' : 'outcome';
     }
 
-    return this.type();
+    return this.type() as CssClass;
   });
 }
