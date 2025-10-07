89a14e3f11796315143f0f17bc59a575cf99c172
Erikson Gonçalves Bezerra <eriksongoncalves@yahoo.com.br>
2025-10-07 18:39:36 -0300

    feat: no transactions component

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -2,12 +2,13 @@ import { Component, signal } from '@angular/core';
 
 import { Balance } from './components/balance/balance';
 import { TransactionItem } from './components/transaction-item/transaction-item';
+import { NoTransactions } from './components/no-transactions/no-transactions';
 import { Transaction } from '../../shared/transaction/interfaces/transaction';
 import { TransactionType } from '../../shared/transaction/enums/transaction-type';
 
 @Component({
   selector: 'app-home',
-  imports: [Balance, TransactionItem],
+  imports: [Balance, TransactionItem, NoTransactions],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
