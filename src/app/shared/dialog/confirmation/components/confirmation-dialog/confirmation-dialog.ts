13fa96de3b320ddd925cded1ef316d784d536a2b
Erikson Gonçalves Sanchez <eriksongoncalves@yahoo.com.br>
2025-10-08 15:57:16 -0300

    feat: override button style

--- src/app/shared/dialog/confirmation/components/confirmation-dialog/confirmation-dialog.ts
+++ src/app/shared/dialog/confirmation/components/confirmation-dialog/confirmation-dialog.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+import { MatButton } from '@angular/material/button';
 
 import { DialogData } from '../../interfaces/dialog-data';
 
 @Component({
   selector: 'app-confirmation-dialog',
-  imports: [MatDialogModule],
+  imports: [MatDialogModule, MatButton],
   templateUrl: './confirmation-dialog.html',
   styleUrl: './confirmation-dialog.scss',
 })
