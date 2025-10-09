072f034c3096a09789e08c32366c38d5232ffd88
Erikson Gonçalves Bezerra <eriksongoncalves@yahoo.com.br>
2025-10-09 20:19:11 -0300

    feat: logout button

--- src/app/core/layout/components/header/header.ts
+++ src/app/core/layout/components/header/header.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
 import { MatToolbar } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbar],
+  imports: [MatToolbar, MatButton],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
